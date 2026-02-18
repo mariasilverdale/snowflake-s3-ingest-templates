c9553aa9a3b2ee6670baf0617386087c85a113b7
Maria Molchanova <mariasilverdale@hotmail.com>
2026-02-19 10:45:13 +1300

    Create 02_describe_integration_get_aws_user_arn.sql

---
+++ 02_snowflake_integrations/02_describe_integration_get_aws_user_arn.sql
@@ -0,0 +1,5 @@
+DESC INTEGRATION s3_ingest_int;
+
+-- In the output, copy these:
+-- 1) STORAGE_AWS_IAM_USER_ARN  -> goes into AWS trust policy as <SNOWFLAKE_IAM_USER_ARN>
+-- 2) STORAGE_AWS_EXTERNAL_ID   -> goes into AWS trust policy as <EXTERNAL_ID>
